3c18ca9c2d30eaec83a8bd980f28bfbcc202039f
Cgarg547 <chiraggargkkp@gmail.com>
2024-08-03 07:13:10 +0000

    initialise billing1.cpp

--- billing1.cpp
+++ billing1.cpp
@@ -19,4 +19,36 @@ class shopping {
         void remove();
         void list();
         void receipt();
+};
+
+void shopping :: menu(){
+    int choice;
+    string email;
+    string password;
+
+    cout << "\t\t\t\t_______________________________________________\n";
+    cout << "\t\t\t\t_______________________________________________\n";
+    cout << "\t\t\t\t            Supermarket Main Menu              \n";
+    cout << "\t\t\t\t_______________________________________________\n";
+    cout << "\t\t\t\t_______________________________________________\n";
+    cout << "\t\t\t\t                                               \n";
+    cout << "\t\t\t\t|    1) Administrator     |\n";
+    cout << "\t\t\t\t                          |\n";
+    cout << "\t\t\t\t|    1) Buyer             |\n";
+    cout << "\t\t\t\t                          |\n";
+    cout << "\t\t\t\t|    1) Exit              |\n";
+    cout << "\n\t\t\t   Please select:";
+    cin >> choice;
+
+    switch(choice){
+        case 1:
+            cout << "\t\t\t Please Login \n";
+            cout << "\t\t\t Enter Email  \n";
+            cin >> email;
+            cout << "\t\t\t Password        \n";
+            cin >> password;
+    }
+
+
+
 }
